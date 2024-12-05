62a7a0622adfef32b3965dec3654b18ab15c31e5
chaeminyu <chaemin.u@gmail.com>
2024-12-05 17:06:46 +0900

    Project Initialization

---
+++ src/main/java/org/zerock/finedu/finedubackend/FinEduBackendApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.finedu.finedubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinEduBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinEduBackendApplication.class, args);
+	}
+
+}
