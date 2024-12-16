3a9eb43dbc8d65171d3b4d53dca2147d7c40f743
chaeminyu <chaemin.u@gmail.com>
2024-12-16 23:31:45 +0900

    feat: summarize 서비스 cronjob

--- summarizer/src/main/java/org/zerock/finedu/summarizer/SummarizerApplication.java
+++ summarizer/src/main/java/org/zerock/finedu/summarizer/SummarizerApplication.java
@@ -4,16 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {
-        "org.zerock.finedu.summarizer",    // quiz 모듈
-        "org.zerock.finedu.common"  // common 모듈
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "org.zerock.finedu.summarizer",
+        "org.zerock.finedu.common"
 })
 @EnableJpaRepositories(basePackages = {
         "org.zerock.finedu.common.repository"  // JPA 리포지토리 위치
 })
 @EntityScan(basePackages = {
-        "org.zerock.finedu.common.entity"  // 엔티티 스캔 추가
+        "org.zerock.finedu.common.entity",    // common 모듈 엔티티
 })
 public class SummarizerApplication {
     public static void main(String[] args) {
